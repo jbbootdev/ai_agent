078e8944f469222107e8c5c97ddc9f2a2d9378bf
James Bunn <madhatter84gn@fastmail.com>
2025-09-07 15:50:01 -0400

    complete system args and handled argument for verbosity

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
+
+if len(sys.argv) < 2:
+    print("You must provide a prompt.")
+    exit(1)
+verbose = sys.argv[2] if len(sys.argv) > 2 and sys.argv[2] == "--verbose" else None
+
+user_prompt = sys.argv[1]
+messages = types.Content(
+    role="user",
+    parts=[
+        types.Part(text=user_prompt),
+    ],
+)
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    model="gemini-2.0-flash-001", contents=messages
 )
 
 print(f"{response.text}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
